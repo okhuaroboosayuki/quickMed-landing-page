1dc493be7c70fdf5091e976c6ab3c8ef39dd01fa
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-10-27 01:48:49 +0000

    style: added Inter font-family, and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/js/*.js"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "blue-gray": "#6C737F",
+        "midnight-charcoal": "#161618",
+        "bright-violet": "#662FFF",
+      },
+    },
   },
   plugins: [],
 };
