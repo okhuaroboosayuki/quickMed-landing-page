af04c7320164f247fbcb1ae5140daddd896b0005
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-10-28 20:54:50 +0100

    feat: added styles & functionalities for mobile menu icon click

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,38 @@
 const initApp = () => {
   const mobileMenuBtn = document.getElementById("mobileMenuIcon");
+  const nav = document.querySelector("#nav");
+  const navList = document.getElementById("nav-list");
+  const btns = document.getElementById("btns");
 
   const toggleMenu = () => {
     mobileMenuBtn.classList.toggle("toggle-menu");
+    nav.classList.toggle("hidden");
+    navList.classList.toggle("open-menu");
+    btns.classList.toggle("show-btns");
+
+    mobileMenuBtn.classList.contains("toggle-menu") ? setDocumentOverFlow(true) : setDocumentOverFlow(false);
   };
 
   mobileMenuBtn.addEventListener("click", toggleMenu);
+
+  window.addEventListener("resize", () => {
+    if (navList.classList.contains("open-menu")) toggleMenu();
+  });
 };
+
+// FUNCTIONS
+
+/**
+ * Sets the overflow property of the document based on the provided boolean value.
+ *
+ * @param {boolean} isOpen - If true, disables scrolling by setting overflow to hidden. If false, restores scrolling by resetting overflow properties.
+ */
+function setDocumentOverFlow(isOpen) {
+  if (isOpen) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+}
 
 document.addEventListener("DOMContentLoaded", initApp);
