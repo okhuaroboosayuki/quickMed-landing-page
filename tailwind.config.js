3741dab856de790ec45a50af1ddbb16817a3581f
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-10-28 20:36:53 +0100

    style: added new custom colors, & a bounce-up animation for mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,22 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
       },
       colors: {
+        Navy: "#141C53",
         "blue-gray": "#6C737F",
         "midnight-charcoal": "#161618",
         "bright-violet": "#662FFF",
+        "charcoal-purple": "#383540",
+        "ice-white": "#F8F9FB",
+      },
+      keyframes: {
+        "bounce-up": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "bounce-up": "bounce-up 0.5s ease-in-out forwards",
       },
     },
   },
