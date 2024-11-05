1451bbc4e1a788730f89f05a60aecf1f925a76e5
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-11-05 15:16:33 +0100

    chore: changed image url

--- build/js/data.js
+++ build/js/data.js
@@ -8,42 +8,42 @@ export const pricing = [
     },
     benefits: [
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "End-to-End Encryption",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "HIPAA Compliance",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Email Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "1,000 Patient Records",
         check: "good",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "Priority Support",
         check: "cancel",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "Advanced Analytics",
         check: "cancel",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "24/7 Dedicated Support",
         check: "cancel",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "Custom Integrations",
         check: "cancel",
       },
@@ -62,42 +62,42 @@ export const pricing = [
     },
     benefits: [
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "End-to-End Encryption",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "HIPAA Compliance",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Email Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "1,000 Patient Records",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Priority Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Advanced Analytics",
         check: "good",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "24/7 Dedicated Support",
         check: "cancel",
       },
       {
-        image: "../images/check-cancel.webp",
+        image: "../icons/check-cancel.webp",
         name: "Custom Integrations",
         check: "cancel",
       },
@@ -112,42 +112,42 @@ export const pricing = [
     },
     benefits: [
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "End-to-End Encryption",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "HIPAA Compliance",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Email Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "1,000 Patient Records",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Priority Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Advanced Analytics",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "24/7 Dedicated Support",
         check: "good",
       },
       {
-        image: "../images/check-good.webp",
+        image: "../icons/check-good.webp",
         name: "Custom Integrations",
         check: "good",
       },
