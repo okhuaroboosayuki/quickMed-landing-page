6f2988ff25560f5b42162b61cac32388738cdcb0
okhuaroboosayuki <osayukiraymond@gmail.com>
2024-11-05 03:01:21 +0100

    feat: created a data file for storing pricing cards info

---
+++ build/js/data.js
@@ -0,0 +1,156 @@
+export const pricing = [
+  {
+    title: "basic",
+    price: "free",
+    button: {
+      bgColor: "#F2F2F2",
+      text: "get started",
+    },
+    benefits: [
+      {
+        image: "../images/check-good.webp",
+        name: "End-to-End Encryption",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "HIPAA Compliance",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Email Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "1,000 Patient Records",
+        check: "good",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "Priority Support",
+        check: "cancel",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "Advanced Analytics",
+        check: "cancel",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "24/7 Dedicated Support",
+        check: "cancel",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "Custom Integrations",
+        check: "cancel",
+      },
+    ],
+  },
+  {
+    title: "professional",
+    price: {
+      amount: "$299",
+      per: "month",
+    },
+    tag: "Most Popular 🔥",
+    button: {
+      bgColor: "#F2F2F2",
+      text: "get started",
+    },
+    benefits: [
+      {
+        image: "../images/check-good.webp",
+        name: "End-to-End Encryption",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "HIPAA Compliance",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Email Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "1,000 Patient Records",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Priority Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Advanced Analytics",
+        check: "good",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "24/7 Dedicated Support",
+        check: "cancel",
+      },
+      {
+        image: "../images/check-cancel.webp",
+        name: "Custom Integrations",
+        check: "cancel",
+      },
+    ],
+  },
+  {
+    title: "enterprise",
+    price: "custom",
+    button: {
+      bgColor: "#F2F2F2",
+      text: "get started",
+    },
+    benefits: [
+      {
+        image: "../images/check-good.webp",
+        name: "End-to-End Encryption",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "HIPAA Compliance",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Email Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "1,000 Patient Records",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Priority Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Advanced Analytics",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "24/7 Dedicated Support",
+        check: "good",
+      },
+      {
+        image: "../images/check-good.webp",
+        name: "Custom Integrations",
+        check: "good",
+      },
+    ],
+  },
+];
